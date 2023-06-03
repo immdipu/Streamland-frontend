2ecedb62ab99a635f96ffecb6ae68e5d0816c7f2
immdipu <dipuchaurasiya91@gmail.com>
2023-06-03 17:11:10 +0545

    sidebar updated

--- src/app/component/sidenav/component/index.tsx
+++ src/app/component/sidenav/component/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Link from "next/link";
 
 const index = () => {
   return (
     <div>
-      <h1 className="text-white">cinemaa</h1>
+      <h1 className="text-_white font-Helvetica text-2xl font-bold tracking-wider text-center mt-8">
+        CINEMAA
+      </h1>
+      <section>
+        <h4>News Feed</h4>
+        <div>
+          <Link>Browse</Link>
+        </div>
+      </section>
     </div>
   );
 };
