0c2c9965ef8dee5c33023989292f2b317a0addbf
immdipu <dipuchaurasiya91@gmail.com>
2023-06-10 14:20:48 +0545

    tooltip added

--- src/app/component/silder/SingleCard.tsx
+++ src/app/component/silder/SingleCard.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Image from "next/image";
 import { NowPlayingResponse } from "@/types/types";
+import Link from "next/link";
+import "react-tooltip/dist/react-tooltip.css";
+import { Tooltip as ReactTooltip } from "react-tooltip";
 
 const SingleCard: React.FC<NowPlayingResponse> = ({
   id,
@@ -19,7 +22,10 @@ const SingleCard: React.FC<NowPlayingResponse> = ({
   vote_count,
 }) => {
   return (
-    <div className=" rounded-lg flex-grow-0 flex-shrink-0">
+    <Link
+      href={`movie/${id}`}
+      className=" rounded-lg flex-grow-0 w-36 flex-shrink-0"
+    >
       <Image
         src={`https://image.tmdb.org/t/p/original/${poster_path!}`}
         width={125}
@@ -28,7 +34,18 @@ const SingleCard: React.FC<NowPlayingResponse> = ({
         style={{ objectFit: "cover" }}
         className="rounded-lg w-36 h-52 select-none"
       />
-    </div>
+      <h3
+        id={`movie${id}`}
+        className="text-sm py-2 whitespace-nowrap overflow-hidden text-ellipsis text-_white"
+      >
+        {title}
+      </h3>
+      <ReactTooltip
+        anchorSelect={`#movie${id}`}
+        place="bottom"
+        content={title}
+      />
+    </Link>
   );
 };
 
---
+++ src/app/movie/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = ({ params }: any) => {
+  return <div>{params.id}</div>;
+};
+
+export default page;
