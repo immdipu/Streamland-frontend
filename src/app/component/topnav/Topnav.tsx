6accc2389a56215eb54fb03e114b10b4fbdf4b40
immdipu <dipu.chaurasiya91@gmail.com>
2023-06-10 08:29:32 +0545

    searchbar added

--- src/app/component/topnav/Topnav.tsx
+++ src/app/component/topnav/Topnav.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import SearchStatic from "./SearchStatic";
 const Topnav = () => {
   return (
-    <div className="pl-56 h-full sticky top-0 bg-transparent">
-      <section className="bg-_black_bg h-20 flex items-center w-full px-16">
+    <div className="pl-56 h-full z-50 sticky top-0 ">
+      <section className="bg-_black_bg backdrop-blur-md h-20 flex items-center w-full px-16">
         <SearchStatic />
       </section>
     </div>
