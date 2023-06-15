bc4cf363b29ae6b703e7a9910f9d0bbe861f4e8c
immdipu <dipuchaurasiya91@gmail.com>
2023-06-15 07:51:35 +0545

    bug fixed

--- src/context/GlobalProvider.tsx
+++ src/context/GlobalProvider.tsx
@@ -2,15 +2,9 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
-import NextNProgress from "nextjs-progressbar";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <Provider store={store}>
-      <NextNProgress />
-      {children}
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 };
 
 export default Providers;
