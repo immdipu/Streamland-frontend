a4f7274c440053b7bbedc7dd886bb13818fa207e
immdipu <dipuchaurasiya91@gmail.com>
2023-06-19 23:00:15 +0545

    tab id added to url

--- src/app/component/genres/MovieGenreList.tsx
+++ src/app/component/genres/MovieGenreList.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { genres } from "@/utils/genreData";
 import clsx from "clsx";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const MovieGenreList = () => {
@@ -16,7 +17,8 @@ const MovieGenreList = () => {
       <ul className="flex flex-wrap gap-3 pl-16 mt-6">
         {genres.map((item) => {
           return (
-            <li
+            <Link
+              href={`movie/genre/?tab=${item.id}`}
               key={item.id}
               className={clsx(
                 " px-2 text-sm py-1 cursor-pointer hover:text-_sidenav_bg hover:bg-_blue duration-200 transition-all ease-linear hover:shadow-lg rounded-lg",
@@ -29,7 +31,7 @@ const MovieGenreList = () => {
               }}
             >
               {item.name}
-            </li>
+            </Link>
           );
         })}
       </ul>
