e23896583e8a5aa13381dfce36dfcc93c4a7ff60
Dipu <dipuchaurasiya91@gmail.com>
2023-07-18 22:23:52 +0545

    design improved

--- src/app/component/login/Login.tsx
+++ src/app/component/login/Login.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
   return (
@@ -13,18 +14,21 @@ const Login = () => {
               Watch movies and tv shows, save your favorite movie to watch later
             </p>
             <p className="flex flex-col items-center justify-center mt-10 text-center">
-              <span> Don&apost have an account?</span>
-              <a href="#" className="underline">
+              <span> Don&apos;t have an account?</span>
+              <a
+                href="#"
+                className="underline hover:text-neutral-200 underline-offset-4 "
+              >
                 Get Started!
               </a>
             </p>
             <p className="mt-6 text-sm text-center text-gray-300">
               Read our{" "}
-              <a href="#" className="underline">
+              <a href="#" className="underline underline-offset-2 text-white">
                 terms
               </a>{" "}
               and{" "}
-              <a href="#" className="underline">
+              <a href="#" className="underline text-white underline-offset-2">
                 conditions
               </a>
             </p>
@@ -39,13 +43,13 @@ const Login = () => {
                   htmlFor="email"
                   className="text-sm font-medium tracking-wide text-neutral-200"
                 >
-                  Email address
+                  Email or username
                 </label>
                 <input
                   type="email"
                   id="email"
                   autoFocus
-                  className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+                  className="px-4 py-2 transition duration-300 border border-neutral-400 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
                 />
               </div>
               <div className="flex flex-col space-y-1">
@@ -66,64 +70,36 @@ const Login = () => {
                 <input
                   type="password"
                   id="password"
-                  className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+                  className="px-4 py-2 transition duration-300 border border-neutral-400 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
                 />
               </div>
               <br />
               <div className="mt-2">
                 <button
                   type="submit"
-                  className="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
+                  className="w-full px-4 py-2 text-lg font-medium tracking-wide text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
                 >
                   Log in
                 </button>
               </div>
               <div className="flex flex-col space-y-5 ">
                 <span className="flex items-center justify-center space-x-2">
-                  <span className="h-px bg-gray-400 w-14"></span>
-                  <span className="font-normal text-neutral-300">
-                    or login with
+                  <span className="h-px bg-gray-400 bg-opacity-30 w-full"></span>
+                  <span className="font-normal text-neutral-300 whitespace-nowrap">
+                    Or continue with
                   </span>
-                  <span className="h-px bg-gray-400 w-14"></span>
+                  <span className="h-px bg-gray-400 w-full bg-opacity-30"></span>
                 </span>
                 <div className="flex flex-col space-y-4">
                   <a
                     href="#"
-                    className="flex items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-gray-800 rounded-md group hover:bg-gray-800 focus:outline-none"
+                    className="flex  items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-neutral-400 rounded-md group hover:bg-neutral-200 focus:outline-none"
                   >
                     <span>
-                      <svg
-                        className="w-5 h-5 text-gray-800 fill-current group-hover:text-white"
-                        viewBox="0 0 16 16"
-                        version="1.1"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-                        ></path>
-                      </svg>
+                      <FcGoogle className="text-xl" />
                     </span>
-                    <span className="text-sm font-medium text-neutral-300 group-hover:text-white">
+                    <span className="text-base tracking-wide font-medium text-neutral-300 group-hover:text-neutral-700">
                       Google
-                    </span>
-                  </a>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-blue-500 rounded-md group hover:bg-blue-500 focus:outline-none"
-                  >
-                    <span>
-                      <svg
-                        className="text-blue-500 group-hover:text-white"
-                        width="20"
-                        height="20"
-                        fill="currentColor"
-                      >
-                        <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"></path>
-                      </svg>
-                    </span>
-                    <span className="text-sm font-medium text-blue-500 group-hover:text-white">
-                      Twitter
                     </span>
                   </a>
                 </div>
