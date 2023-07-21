2b6738d390c38db4931a902972d2433044943ab4
Dipu <dipuchaurasiya91@gmail.com>
2023-07-21 18:12:55 +0545

    meta updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,8 @@ export const metadata = {
   title: "Cinemaa",
   description:
     "CINEMAA is a website where you can stream different movies and tv shows for free. We do not store any copyright-protected content on our website. Any linked content is stored only in third-party websites. This is a promotional website only. All files placed here are for introducing purpose.",
-  generator: "Next.js",
   applicationName: "Cinemaa",
   referrer: "origin-when-cross-origin",
-  keywords: ["Next.js", "React", "JavaScript"],
   colorScheme: "dark",
   monetag: "7bc7a88836ba455ca44573ef4f1f8f5a",
 };
