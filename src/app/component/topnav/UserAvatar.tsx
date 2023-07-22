a2db396afcd5211f29f371a5db99a79193e2059c
Dipu <dipuchaurasiya91@gmail.com>
2023-07-22 10:25:55 +0545

    user avatar added

--- src/app/component/topnav/UserAvatar.tsx
+++ src/app/component/topnav/UserAvatar.tsx
@@ -1,7 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
 import React from "react";
+import { useAppSelector } from "@/redux/hooks";
+import Image from "next/image";
 
 const UserAvatar = () => {
-  return <div>UserAvatar</div>;
+  const user = useAppSelector((state) => state.auth);
+  return (
+    <div>
+      <div className="bg-neutral-500 p-[2px] rounded-md">
+        {user.profilePic && (
+          <img
+            className="w-10 h-10 rounded-md object-cover"
+            src={user.profilePic}
+            alt="user profile"
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default UserAvatar;
