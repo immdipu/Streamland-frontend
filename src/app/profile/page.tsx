b0e164a7a6afd5b9916c5b8e833d362a8e11a497
Dipu <dipuchaurasiya91@gmail.com>
2023-09-07 07:32:20 +0545

    profile page redirect added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Profile from "../component/profile/Profile";
 import { getUserDataTypes } from "@/types/userTypes";
+import { redirect } from "next/navigation";
 
 const page = async () => {
-  return <div className="mt-20">hello</div>;
+  redirect("/");
+  return <div className="mt-20"></div>;
 };
 
 export default page;
