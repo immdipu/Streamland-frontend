4acacbe9ed5060036822fdf575e955cac4070e27
Dipu <dipuchaurasiya91@gmail.com>
2023-11-08 12:37:34 +0545

    animation added to the watchlist

--- src/app/component/watchlist/Watchlist.tsx
+++ src/app/component/watchlist/Watchlist.tsx
@@ -9,9 +9,11 @@ import LoadingAnimation from "./LoadingAnimation";
 import { AddMediaResponse } from "@/types/userTypes";
 import { useRouter, useSearchParams } from "next/navigation";
 import clsx from "clsx";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const Watchlist = () => {
   const user = useAppSelector((state) => state.auth);
+  const [parent, enableAnimations] = useAutoAnimate(/* optional config */);
   const searchParams = useSearchParams();
   const router = useRouter();
   const { isLoading, data, error } = useQuery(
@@ -88,31 +90,36 @@ const Watchlist = () => {
         </button>
       </section>
 
-      {searchParams.get("tab") === "watchlist" ? (
-        <>
-          {data.watchlist.length > 0 &&
-            data.watchlist.map((item: AddMediaResponse, index: number) => {
-              return <MoviesList key={item.id} {...item} Index={index} />;
-            })}
-        </>
-      ) : (
-        <>
-          {data.history.length > 0 &&
-            data.history.map((item: AddMediaResponse, index: number) => {
-              return <MoviesList key={item.id} {...item} Index={index} />;
-            })}
-        </>
-      )}
+      <section ref={parent}>
+        {searchParams.get("tab") === "watchlist" ? (
+          <>
+            {data.watchlist.length > 0 &&
+              data.watchlist.map((item: AddMediaResponse, index: number) => {
+                return <MoviesList key={item.id} {...item} Index={index} />;
+              })}
+          </>
+        ) : (
+          <>
+            {data.history.length > 0 &&
+              data.history.map((item: AddMediaResponse, index: number) => {
+                return <MoviesList key={item.id} {...item} Index={index} />;
+              })}
+          </>
+        )}
+      </section>
       <section>
-        {data.watchlist.length === 0 && (
+        {data.watchlist.length === 0 || data.history.length === 0 ? (
           <div className=" w-full h-96 grid place-content-center ">
             <p className="text-xl text-neutral-500">
-              {searchParams.get("tab") === "watchlist"
-                ? "You have no movies in your watchlist."
-                : "You have no movies in your history."}
+              {searchParams.get("tab") === "watchlist" &&
+                data.watchlist.length === 0 &&
+                "Your watchlist is empty."}
+              {searchParams.get("tab") === "history" &&
+                data.history.length === 0 &&
+                "No history found."}
             </p>
           </div>
-        )}
+        ) : null}
       </section>
     </div>
   );
